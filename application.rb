8f3ba9e2b9e8c3735a997f88fe1b012bbcf0aa7e
issyl0 <isabell@issyl0.co.uk>
2013-11-23 18:20:57 +0000

    Query the API with a few parameters and the beginnings of the required data, to make sure it works. ;-)

--- application.rb
+++ application.rb
@@ -3,5 +3,8 @@ require 'shotgun'
 require 'rest-client'
 
 get '/' do
+  gtr_root = "http://gtr.rcuk.ac.uk:80/gtr/api/"
+  api_response = RestClient.get(gtr_root + "projects?size=120", :accept => 'application/vnd.rcuk.gtr.json-v1')
+  p JSON.parse(api_response)
   erb :index
 end
