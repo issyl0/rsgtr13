687ef59b0c5cb2f8a97f525bd05b76a47cee8aed
issyl0 <isabell@issyl0.co.uk>
2013-11-24 16:44:35 +0000

    Half-working, half-static code for demo purposes.

--- application.rb
+++ application.rb
@@ -1,10 +1,55 @@
 require 'sinatra'
 require 'shotgun'
 require 'rest-client'
+require 'pp'
+require 'geocoder'
+
+#    for each project, get its organisation
+#     for each organisation, get its postcode
+#       then plot the postcode on the map, and it should display every project's location on the map (with clustering)
+
+before do
+  def recursive_symbolize_keys(h)
+    case h
+    when Hash
+      Hash[
+        h.map do |k, v|
+          [ k.respond_to?(:to_sym) ? k.to_sym : k, recursive_symbolize_keys(v) ]
+        end
+      ]
+    when Enumerable
+      h.map { |v| recursive_symbolize_keys(v) }
+    else
+      h
+    end
+  end
+end
 
 get '/' do
-  gtr_root = "http://gtr.rcuk.ac.uk:80/gtr/api/"
-  api_response = RestClient.get(gtr_root + "projects?size=120", :accept => 'application/vnd.rcuk.gtr.json-v1')
-  p JSON.parse(api_response)
+
+  @org_postcodes = Array.new()
+  gtr_root = "http://gtr.rcuk.ac.uk:80/gtr/api"
+  api_response = RestClient.get(gtr_root + "/projects?s=100", :accept => 'application/vnd.rcuk.gtr.json-v1')
+  projects = JSON.parse(api_response)
+  projects = recursive_symbolize_keys(projects)
+
+  projects[:project].each do |i|
+    i[:links][:link].each do |j|
+      if j[:rel] == "LEAD_ORG"
+      @org_url = j[:href]
+      end
+    end
+
+    api_response = RestClient.get(@org_url, :accept => 'application/vnd.rcuk.gtr.json-v1')
+    @org = JSON.parse(api_response)
+    @org = recursive_symbolize_keys(@org)
+    @org_postcodes.push(@org[:addresses][:address][0][:postCode])
+  end
+  pp @org_postcodes
+  @org_postcodes.each do |k|
+    @latlng = Geocoder.coordinates(k)
+    pp @latlng
+  end
+
   erb :index
 end
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,14 +6,39 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-  // Random latitudes and longitudes for now.
-  var research_points = [["52.48278", "1.40625"], ["52.908902", "-2.373047"], ["51.086126", "-2.210767"], ["51.511214", "-0.119824"]];
+  var markers = [];
+  var research_points = [
+                          ["50.934189", "-1.3956847"],
+                          ["51.4697688", "-3.1649216"],
+                          ["55.9476915", "-3.187347"],
+                          ["52.62187429999999", "1.2223058"],
+                          ["51.4697688", "-3.1649216"],
+                          ["51.5005046", "-0.1782187"],
+                          ["52.0249136", "-0.7097473999999999"],
+                          ["52.44897169999999", "-1.9308602"],
+                          ["53.7613383", "-2.7073841"],
+                          ["51.520925", "-0.1306868"],
+                          ["52.9555003", "-1.1432387"],
+                          ["51.5030338", "-0.0852242"],
+                          ["56.1492131", "-3.9267942"],
+                          ["51.5518193", "-0.1102363"],
+                          ["54.01046849999999", "-2.7869096"],
+                          ["53.8084778", "-1.5527923"],
+                          ["51.5247725", "-0.1334268"],
+                          ["53.39482719999999", "-1.4306072"],
+                          ["54.7749254", "-1.5697502"],
+                          ["53.4063986", "-2.975554"]
+                        ];
+
+  // Iterate the array of postcodes, for clustered markers.
   for (var i = 0; i < research_points.length; i++) {
     var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(research_points[i][0], research_points[i][1]),
-      map: map
+      map: map,
+      position: new google.maps.LatLng(research_points[i][0], research_points[i][1])
     });
+    markers.push(marker);
   }
+  var cluster = new MarkerClusterer(map, markers);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
